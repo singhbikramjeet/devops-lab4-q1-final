71f99820e1ba9a645f51fc4537d5b684039c3372
Bikramjeet Singh <b102@my.centennialcollege.ca>
2023-04-13 22:42:10 -0400

    adding lab4 q1 files

---
+++ ShowCase/Program.cs
@@ -0,0 +1,39 @@
+﻿using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("Entered String Starts with uppercase? " +
+                 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+            Console.WriteLine();
+            row += 2;
+        } while (true);
+
+        return;
+
+        // Declare a ResetConsole local method
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue......");
+                Console.ReadKey();
+            }
+
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Enter a string input and press enter:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
